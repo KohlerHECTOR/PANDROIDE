583852f73bfce04e593de303249e2c98a7038a1e
KohlerHECTOR <77987579+KohlerHECTOR@users.noreply.github.com>
2021-02-08 22:32:58 +0100

    Add files via upload

--- policies/__init__.py
+++ policies/__init__.py
@@ -5,3 +5,4 @@ from policies.squashed_gaussian_policy import SquashedGaussianPolicy
 from policies.cnn_policy import CnnPolicy
 from policies.generic_net import GenericNet
 from policies.policy_wrapper import PolicyWrapper
+from policies.bernoullI_for_cem_policy import BernoulliCEM
--- policies/bernoullI_for_cem_policy.py
+++ policies/bernoullI_for_cem_policy.py
@@ -18,7 +18,7 @@ def make_det_vec(values):
         else:
             choices.append(0.0)
     return choices
-class bernoulliCEM(GenericNet):
+class BernoulliCEM(GenericNet):
     """
     A policy whose probabilistic output is a boolean value for each state
     """
@@ -31,10 +31,10 @@ class bernoulliCEM(GenericNet):
         # self.fc2.weight.data.uniform_(-1.0, 1.0)
         self.fc3 = nn.Linear(l3, l4)  # Prob of Left
         # self.fc3.weight.data.uniform_(-1.0, 1.0)
-        s_size = l1
-        h1_size = l2
-        h2_size = l3
-        a_size = l4
+        self.s_size = l1
+        self.h1_size = l2
+        self.h2_size = l3
+        self.a_size = l4
     def set_weights(self, weights):
         s_size = self.s_size
         h1_size = self.h1_size
@@ -44,9 +44,9 @@ class bernoulliCEM(GenericNet):
         fc1_end = (s_size*h1_size)+h1_size
         fc1_W = torch.from_numpy(weights[:s_size*h1_size].reshape(s_size, h1_size))
         fc1_b = torch.from_numpy(weights[s_size*h1_size:fc1_end])
-        fc2_end = (h1_size*h2_size)+h2_size
-        fc2_W = torch.from_numpy(weights[:h1_size*h2_size].reshape(h1_size, h2_size))
-        fc2_b = torch.from_numpy(weights[h1_size*h2_size:fc2_end])
+        fc2_end = fc1_end+(h1_size*h2_size)+h2_size
+        fc2_W = torch.from_numpy(weights[fc1_end:fc1_end+(h1_size*h2_size)].reshape(h1_size, h2_size))
+        fc2_b = torch.from_numpy(weights[fc1_end+(h1_size*h2_size):fc2_end])
         fc3_W = torch.from_numpy(weights[fc2_end:fc2_end+(h2_size*a_size)].reshape(h2_size, a_size))
         fc3_b = torch.from_numpy(weights[fc2_end+(h2_size*a_size):])
         # set the weights for each layer
@@ -54,8 +54,8 @@ class bernoulliCEM(GenericNet):
         self.fc1.bias.data.copy_(fc1_b.view_as(self.fc1.bias.data))
         self.fc2.weight.data.copy_(fc2_W.view_as(self.fc2.weight.data))
         self.fc2.bias.data.copy_(fc2_b.view_as(self.fc2.bias.data))
-        self.fc3.weight.data.copy_(fc2_W.view_as(self.fc2.weight.data))
-        self.fc3.bias.data.copy_(fc2_b.view_as(self.fc2.bias.data))
+        self.fc3.weight.data.copy_(fc3_W.view_as(self.fc3.weight.data))
+        self.fc3.bias.data.copy_(fc3_b.view_as(self.fc3.bias.data))
 
 
     def get_weights_dim(self):
@@ -74,7 +74,7 @@ class bernoulliCEM(GenericNet):
         action = torch.sigmoid(self.fc3(state))
         return action #paramtre de la bernoulli
 
-    def select_action(self, state, deterministic=False):
+    def select_action(self, state, deterministic):
         """
         Compute an action or vector of actions given a state or vector of states
         :param state: the input state(s)
@@ -88,5 +88,18 @@ class bernoulliCEM(GenericNet):
             else:
                 action = Bernoulli(probs).sample()
             return action.data.numpy().astype(int)
-    def train_cem():
-        x=x
+
+    def train_cem(self, state, action, reward):
+        """
+        Train the policy using a policy gradient approach
+        :param state: the input state(s)
+        :param action: the input action(s)
+        :param reward: the resulting reward
+        :return: the loss applied to train the policy
+        """
+        action = torch.FloatTensor(action)
+        reward = torch.FloatTensor(reward)
+        probs = self.forward(state)
+        m = Bernoulli(probs)
+        loss = -m.log_prob(action).sum(dim=-1) * reward  # Negative score function x reward
+        return loss
