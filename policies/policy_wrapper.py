08e5ef706cb2d8c8be3721aeed0b09ddeceeb573
KohlerHECTOR <>
2021-04-27 15:36:38 +0200

    for new plots

--- policies/policy_wrapper.py
+++ policies/policy_wrapper.py
@@ -37,23 +37,8 @@ class PolicyWrapper:
         directory = os.getcwd() + '/data/policies/'
         name = self.env_name + '#' + method + '#' + str(self.repet) +'#'+str(best_cycle)+'#' +self.team_name + '_' + str(best_score) \
                    + '#' + self.policy_type + '#' + str(self.max_steps)+ '#' + str(best_score) + '#'
-<<<<<<< HEAD
         os.rename(directory + name+'.pt', directory + name + "BEST" + '#'+'.pt')
 
-=======
-        if os.path.isfile(directory + name+'.pt'):
-            os.rename(directory + name+'.pt', directory + name + "BESTRUN" + '#'+'.pt')
-        else:
-            for file1 in os.listdir(directory):
-                filesplit1 = file1[:-3].split('#')
-                for file2 in os.listdir(directory):
-                    filesplit2 = file2[:-3].split('#')
-                    if filesplit1[6] < filesplit2[6]:
-                        bestfile = file1
-                    else:
-                        bestfile = file2
-            os.rename(directory +bestfile, directory + bestfile[:-3] + "BESTRUNSAVED" + '#'+'.pt')
->>>>>>> 772176ee00f936d00e54666adcfc66a746bcc017
 
 
     def load(self, filename):
